24e6c412c1331c3f2c1ad172453a6bf4e298a427
saimtreloy <streloy@yahoo.com>
2018-03-08 16:49:33 +0600

    2018-03-08 04:40:00 PM

--- app/src/main/java/saim/com/nowagent/Login.java
+++ app/src/main/java/saim/com/nowagent/Login.java
@@ -3,9 +3,14 @@ package saim.com.nowagent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import saim.com.nowagent.Firebase.MyFirebaseInstanceIdService;
 
 public class Login extends AppCompatActivity {
 
@@ -17,7 +22,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        String s = FirebaseInstanceId.getInstance().getToken();
+        Log.d("SAIM", s);
+
         init();
+
+
     }
 
     private void init() {
